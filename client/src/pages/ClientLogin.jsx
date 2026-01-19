dd34b1fb9ddc7233830986d3ecde64b3b4e98cc2
Gustavo Cabello Santelices <154566409+gacz1998@users.noreply.github.com>
2026-01-19 19:40:42 -0300

    Update ClientLogin.jsx

--- client/src/pages/ClientLogin.jsx
+++ client/src/pages/ClientLogin.jsx
@@ -11,7 +11,7 @@ const ClientLogin = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/client/login', { 
+      const res = await axios.post('https://tesis-gustavo-cabello-iplacex-1.onrender.com/api/auth/client/login', { 
         rut: rut.trim(), 
         password: password 
       });
